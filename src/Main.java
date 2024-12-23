b56038acc3b73f0267ac7601060c162103eabc10
master <malofeev1506@gmail.com>
2024-12-23 19:41:08 +0400

    hw-12

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        System.out.println(" Задание 1,2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = " Ivanov Ivan Ivanovich";
+        fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника " + fullName.toUpperCase());
+
+        System.out.println(" Задача 3");
+        String fullNameTwo = " Иванов Семён Семёнович";
+        fullNameTwo = fullNameTwo.replace("ё", "е");
+        System.out.println(fullNameTwo);
+
 
     }
 }
